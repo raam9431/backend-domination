716829a3f1edecb05c1b8350f9594f3ec32867d8
raam9431 <118127013+raam9431@users.noreply.github.com>
2024-09-26 01:58:25 +0530

    15th day commit

--- 8th lesson/script.js
+++ 8th lesson/script.js
@@ -12,9 +12,56 @@
 //     else console.log(data);
 // })
 
-const fs = require('fs');
+// const fs = require('fs');
 
-fs.appendFile("abcd.txt", " appended data", function (err) { 
-    if(err) console.log(err);
-    else console.log("appended data");
-});
+// fs.appendFile("abcd.txt", " appended data", function (err) { 
+//     if(err) console.log(err);
+//     else console.log("appended data");
+// });
+
+// const { log } = require('console');
+// const fs = require('fs');
+
+// fs.rename("abcd.txt", "hey.txt", function (err) {
+//     if (err) console.log(err);
+//     else console.log("file is renamed");
+// })
+
+
+// const fs = require('fs');
+
+// fs.unlink("cbfn.txt", function (err) {
+//     if (err) console.log(err);
+//     else console.log("unliked file(deleted)");   (deleting the file that is linked/name given)
+// })
+
+// const fs = require('fs');
+
+// fs.mkdir("lolo.txt", function (err) { 
+//     if(err) console.log(err);
+//     else console.log("folder/dir is create");
+// })
+
+// const fs = require('fs');
+
+// fs.readdir("lolo", { withFileTypes: true }, function (err, files) {
+//     if (err) console.log(err);
+//     else console.log(files);
+// })
+
+// const fs = require('fs');
+
+// fs.rmdir("hobo",{recursive: true}, function(err){
+//     if(err) console.log(err);
+//     else console.log("file is removed");
+// })
+
+// const fs = require('fs');
+
+// fs.writeFileSync("hey.txt", "whatever");
+// console.log("created");
+
+// let data = fs.readFileSync("hey.txt", "utf8");
+// console.log("read");
+
+
